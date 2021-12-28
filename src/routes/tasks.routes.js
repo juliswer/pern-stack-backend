7d790000f39af9cea56a2a66bfa9c450cd3c0199
Julian Swerdlin <julianswer36@gmail.com>
2021-12-29 00:07:28 +0100

    uploading new files

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const pool = require('../db');
 
 const router = Router();
 
 router.get('/tasks', async (req, res) => {
-    res.send('retrieven a list of task')
+    const result = await pool.query('SELECT NOW()')
+    res.json(result.rows[0].now)
 })
 
 router.get('/tasks/10', (req, res) => {
