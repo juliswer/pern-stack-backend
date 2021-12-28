2f2945b4898b0cae4ab955587d3258f6bc734294
Julian Swerdlin <julianswer36@gmail.com>
2021-12-28 23:19:49 +0100

    new files

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -2,8 +2,24 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World');
+router.get('/tasks', (req, res) => {
+    res.send('Return all tasks');
+})
+
+router.get('/tasks/10', (req, res) => {
+    res.send('Return only one task');
+})
+
+router.post('/tasks', (req, res) => {
+    res.send('Creating a new task');
+})
+
+router.delete('/tasks', (req, res) => {
+    res.send('Deleting a task');
+})
+
+router.put('/tasks', (req, res) => {
+    res.send('Updating a task');
 })
 
 module.exports = router;
